7ecfac57cbdf6fdd6d50a91188c442f81b39f45c
123shanawaz <shanawazm8276@gmail.com>
2023-04-11 20:33:19 +0530

    modified data

--- src/data.js
+++ src/data.js
@@ -48,6 +48,13 @@ const Data = [
 		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
 		price: "68,595",
 	},
+	{
+		id: 8,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: "https://cdn.thecodehelp.in/Agra.jpeg",
+		price: "35,758",
+	},
 ];
 
 export default Data;
